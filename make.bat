366307a5a355000aebebb8ebd90a7609314eee95
dejean <dejean>
2008-01-08 11:20:37 +0000

    addedd libxml et al dependencied for ConstantsXXX.cc

--- make.bat
+++ make.bat
@@ -2,22 +2,22 @@
 set DIRSRC=.\src
 set DIREXE=.\exe
 
-set DIRXPDF= xpdf_3.01\xpdf
-set DIRGOO=xpdf_3.01\goo
-set DIRFOFI=xpdf_3.01\fofi
-set DIRLIBXML=libxml2-2.6.19+.win32
-set DIRICONV=iconv-1.9.1.win32
+set DIRXPDF= ..\pdf2xml\xpdf_3.01\xpdf
+set DIRGOO=..\pdf2xml\xpdf_3.01\goo
+set DIRFOFI=..\pdf2xml\xpdf_3.01\fofi
+set DIRLIBXML=..\pdf2xml\libxml2-2.6.19+.win32
+set DIRICONV=..\pdf2xml\iconv-1.9.1.win32
 set CC=c:\mingw\bin\gcc.exe
 set CFLAGS=-g -O2 -I %DIRSRC% -I %DIRFOFI% -I %DIRGOO%
 set CXX=c:\mingw\bin\g++.exe
 set CXXFLAGS=%CFLAGS%
 set LIBPROG=c:\mingw\bin\ar.exe
 
-copy xpdf_3.01\aconf-dj.h %DIRSRC%\aconf.h
+copy ..\pdf2xml\xpdf_3.01\aconf-dj.h %DIRSRC%\aconf.h
 
-%CXX% %CXXFLAGS% -c %DIRSRC%\ConstantsUtils.cc
-%CXX% %CXXFLAGS% -c %DIRSRC%\ConstantsXML.cc
-%CXX% %CXXFLAGS% -c %DIRSRC%\Parameters.cc
+%CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsUtils.cc
+%CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsXML.cc
+%CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\Parameters.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\AnnotsXrce.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
